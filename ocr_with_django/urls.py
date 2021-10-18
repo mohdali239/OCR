7d5a25f106be6827fa282848b186c815ab7845e3
mohdali239 <61471324+mohdali239@users.noreply.github.com>
2021-10-18 11:33:59 +0530

    Add files via upload

---
+++ ocr_with_django/urls.py
@@ -0,0 +1,25 @@
+"""ocr_with_django URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+from documents.views import ocr_view, ocr_form_view
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^ocr/', ocr_view, name='ocr_view'),
+    url(r'^$', ocr_form_view, name='ocr_form_view'),
+]
